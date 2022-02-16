5f7b3033ee0d2dadabb95bb3f673571a234e2d09
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-16 10:30:16 -0300

    entrega requisitos 8

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = function paintBlack(){
+    let corInicial = localStorage.getItem('first option');
+
+}
+// função para criar pixels
 function createPixels() {
     // onde?
     let linhas = document.getElementsByClassName('line');
@@ -17,6 +22,7 @@ function createPixels() {
 }
 createPixels();
 
+// deu errado
 /* function mudaMedidas() {
     // onde
     
@@ -33,38 +39,75 @@ createPixels();
 }
 mudaMedidas(); */
 
+// função para deixar paleta preta como selecionada
 function initialSelected() {
     const classSelected = 'color selected';
     let color = document.querySelectorAll('.color');
     for (let i = 0; i < color.length; i += 1) {
         if (color[i].style.backgroundColor === 'black') {
             color[i].className = classSelected;
+            const primaCor = color[i];
+            localStorage.setItem('first option', 'primaCor');
         }
     }
 }
 initialSelected();
 
-function selectedColor() {
+// função para clicar paleta e deixar selecionado
+/* function selectedColor()  */
     let color = document.getElementsByClassName('color');
     let selcionado = 'color selected';
     let naoselecionado = 'color';
-    console.log(color[1]);
+    let u;
+
     //passar pelas cores
     for (let i = 0; i < color.length; i += 1) {
         for (let j = 0; j < color.length; j += 1) {
 
         
-    // fazer um event
+    // fazer um event com a paleta
     color[i].addEventListener('click', function(){
         if (color[i] !== color[j]) {
             color[i].className = selcionado;
             color[j].className = naoselecionado;
+            u = color[i].style.backgroundColor;
         } else {
             color[i].className = selcionado;
             color[j].className = selcionado;
-        }
+            u = color[i].style.backgroundColor;
+        } 
     })
     }
 }
+    
+
+let pixelsBox = document.getElementsByClassName('pixel');
+// passa pelos pixels
+for (let i = 0; i < pixelsBox.length; i += 1) {
+    // cria um evento para os pixels
+    pixelsBox[i].addEventListener('click', function(){   
+        // teste
+        for (let j = 0; j < color.length; j += 1) {
+        if (color[0].className === 'color selected') {
+            pixelsBox[i].style.backgroundColor = 'black';
+        }  else {
+    pixelsBox[i].style.backgroundColor = u;
+        }
+    }
+})
 }
-selectedColor();
+
+
+
+/*  function paintPixel()
+    let colorSelected = document.getElementsByClassName('selected');
+    let pixelsBox = document.getElementsByClassName('pixel')
+    console.log(colorSelected);
+    let backgroundColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+    console.log(backgroundColor);
+
+    for (let i = 0; i < pixelsBox.length; i += 1) {
+        pixelsBox[i].addEventListener('click', function(){
+            pixelsBox[i].style.backgroundColor = backgroundColor;
+        })
+    } */
